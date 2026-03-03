138f0d9f25f099dc807af583e743216936f3cab2
Juan Pablo <jzuluagaga@unal.edu.co>
2026-03-02 21:35:36 -0500

    feat: initial project setup con Spring Boot 3.5.11

---
+++ src/main/java/com/hanzi/HanziApplication.java
@@ -0,0 +1,13 @@
+package com.hanzi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanziApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanziApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hanzi/HanziApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanzi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanziApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
